460477094da99925eefcd35de0916c3d5f10dcb9
TungSmile <tungmonkey1101@gmail.com>
2023-10-25 16:19:36 +0700

    done day1-3

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import {Route, Routes} from "react-router";
 import Layout from "./components/Layout";
+import Register from "./register/Register";
 
 function App() {
   return (
     <>
       <Routes>
-          <Route path={"/"} element={<Layout/>}>
-            </Route>
+          <Route path={"/register"} element={<Register/>}></Route>
+          <Route path={"/"} element={<Layout/>}></Route>
       </Routes>
     </>
   );
